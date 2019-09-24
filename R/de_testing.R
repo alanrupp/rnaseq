57b4a0efa8aa984fa14e5060001886d14efed530
Alan Rupp <alan.rupp@gmail.com>
2019-09-24 15:36:10 -0400

    added cpm_means

--- R/de_testing.R
+++ R/de_testing.R
@@ -1,4 +1,18 @@
 library(reticulate)
+
+# - CPM means -----------------------------------------------------------------
+cpm_means <- function(cpm, info, group) {
+  map(unique(as.data.frame(info)[, group]),
+        ~ rowMeans(log2(cpm[, filter(info, !!sym(group) == .x)$Sample_ID] + 1))
+        ) %>%
+    as.data.frame() %>%
+    set_names(unique(as.data.frame(info)[, group])) %>%
+    mutate_all(~ 2^.x - 1) %>%
+    as.data.frame() %>%
+    mutate("gene_id" = cpm$gene_id) %>%
+    select(gene_id, unique(as.data.frame(info)[, group]))
+}
+
 # - Intermine -----------------------------------------------------------------
 intermine <- function(genes) {
   source_python("~/Programs/rnaseq/python/mousemine.py")
@@ -7,3 +21,5 @@ intermine <- function(genes) {
   result <- result[!duplicated(result), ]
   return(result)
 }
+
+
