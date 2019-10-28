2bd9bc778443788e31df5d615998eba6ef378456
Alan Rupp <alan.rupp@gmail.com>
2019-10-28 11:03:51 -0400

    updaed cpm_means to handle 1 sample groups

--- R/de_testing.R
+++ R/de_testing.R
@@ -2,15 +2,22 @@ library(reticulate)
 
 # - CPM means -----------------------------------------------------------------
 cpm_means <- function(cpm, info, group) {
-  map(unique(as.data.frame(info)[, group]),
-        ~ rowMeans(log2(cpm[, filter(info, !!sym(group) == .x)$Sample_ID] + 1))
-        ) %>%
-    as.data.frame() %>%
-    set_names(unique(as.data.frame(info)[, group])) %>%
-    mutate_all(~ 2^.x - 1) %>%
-    as.data.frame() %>%
-    mutate("gene_id" = cpm$gene_id) %>%
-    select(gene_id, unique(as.data.frame(info)[, group]))
+  unique_groups <- unique(info[[group]])
+  mean_vals <- function(x) {
+    df <- cpm[, filter(info, !!sym(group) == x)$Sample_ID]
+    if (is.null(ncol(df))) {
+      warning(paste(x, "has only 1 sample. Cannot calculate mean."),
+              call. = FALSE)
+      return(df)
+    } else {
+      return(2^rowMeans(log2(df + 1))-1)
+    }
+  }
+  df <- map(unique_groups, mean_vals) %>% bind_cols()
+  df <- set_names(df, unique_groups)
+  df$gene_id <- cpm$gene_id
+  df <- select(df, gene_id, unique_groups)
+  return(df)
 }
 
 # - Intermine -----------------------------------------------------------------
@@ -22,4 +29,11 @@ intermine <- function(genes) {
   return(result)
 }
 
+unique_groups <- unique(info$Treatment)
+map(unique_groups,
+    ~ apply(., 2, function(x) 
+      mean(log2(cpm[, filter(bead_info, Treatment == x)$Sample_ID] + 1)))
+    )
 
+
+apply(cpm[, filter(bead_info, Treatment == "PBS")$Sample_ID], 1, mean)
